86155428897402f18b880181854a93a03eecf4c6
DaDiCeMa <zaharalex25@gmail.com>
2020-02-16 15:12:58 +0100

    Created Header component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import logo from './logo.svg';
-import './App.css';
-import Footer from './Footer/Footer'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header></Header>
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h2>In progress!</h2>
-       
-      </header>
-      <Footer/>
+        <h2>In progress!</h2>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Header.css";
+import resumeIcon from "../assets/icons/resume.svg";
+import githubIcon from "../assets/icons/github.svg";
+import linkedInIcon from "../assets/icons/linkedin_filled_logo.svg";
+import emailIcon from "../assets/icons/envelope.svg";
+
+const header = () => {
+  return (
+    <div className="header_wrapper">
+      <div className="header_container-options">
+        <a
+          href="../assets/resume/Resume_Zahar_Alexandru_Blue.pdf"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={resumeIcon} className="icon" alt="Resume"></img>
+        </a>
+        <a
+          href="https://github.com/DaDiCeMa"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={githubIcon} className="icon" alt="Github"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/zahar-alex-179082147/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <img src={linkedInIcon} className="icon" alt="LinkedIn"></img>
+        </a>
+        <a
+          href="mailto:zaharalex25@gmail.com?Subject=Hello%20"
+          target="_top"
+          rel="noopener noreferrer"
+        >
+          <img src={emailIcon} className="icon icon-email" alt="Email"></img>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default header;
