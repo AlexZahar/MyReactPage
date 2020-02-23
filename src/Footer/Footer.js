46f0735541c6f64260c933a053a09137801a6f4c
DaDiCeMa <zaharalex25@gmail.com>
2020-02-23 16:09:34 +0100

    added SVG's and restyled header and footer components

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -2,14 +2,17 @@ import React from "react";
 
 import "./Footer.css";
 
-
 const footer = () => {
-    const today = new Date();
-    const currentYear = today.getFullYear(); 
-  
+  const today = new Date();
+  const currentYear = today.getFullYear();
+
   return (
     <div className="footer_wrapper">
-      <div className="copyright_containter"> <p className="copyright_text">Copyright &copy; Alexandru Zahar {currentYear} </p> </div>
+      <div className="copyright_containter">
+        <p className="copyright_text">
+          Alexandru Zahar &#124; {currentYear} &#124; Personal Portfolio
+        </p>
+      </div>
     </div>
   );
 };
